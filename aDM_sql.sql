6738cbb8c0d348f01c3fd9601cf083feb62ea66a
Diego Santos <120533129+madax14@users.noreply.github.com>
2024-11-13 16:38:28 -0300

    Update aDM_sql.sql

--- aDM_sql.sql
+++ aDM_sql.sql
@@ -42,11 +42,11 @@ ALTER TABLE order_item_refund ADD foreign key(order_item_id) references orders_i
 
 SELECT * FROM order_item_refund;
 
-# Removing wrong data from dataset
+# Removing wrong data from the dataset
 DELETE FROM order_item_refund 
 WHERE order_item_refund_id BETWEEN 6 AND 10;
 
-#Creating new table for products
+#Creating a new table for products
 CREATE TABLE products (
 product_id BIGINT,
 created_at DATETIME,
@@ -61,8 +61,22 @@ INSERT INTO products VALUES
 
 SELECT * FROM products;
 
-#Need to add new column in the orders_items table to identify the product
+#Need to add a new column in the orders_items table to identify the product
 ALTER TABLE orders_items ADD product_id BIGINT;
 
 # Populating the data with the product_id
-UPDATE orders_items SET product_id = 1 WHERE order_item_id BETWEEN 1 AND 1000;
+UPDATE orders_items SET product_id = 1 WHERE order_item_id > 0;
+
+# Setting relationship between tables
+# Setting the product_id from the product table AS FOREIGN KEY FOR THE ORDER_ITEMS TABLE 
+ALTER TABLE orders_items ADD FOREIGN KEY(product_id) REFERENCES products(product_id);
+
+# Deleting data that has been imported incorrectly.
+DELETE FROM orders_items
+WHERE order_item_id BETWEEN 2587 AND 3859;
+
+#Imported data through UI Wizard
+# Checking the amount records
+SELECT count(*) FROM order_item_refund;
+SELECT count(*) FROM orders_itemsd;
+
